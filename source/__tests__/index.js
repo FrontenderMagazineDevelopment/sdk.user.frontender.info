f7e45c8de32a1ad2ed245f67d51e56a418cb3476
Anton Nemtsev <thesilentimp@gmail.com>
2017-10-17 00:25:16 +0300

    ISSUE-1 - user sdk

---
+++ source/__tests__/index.js
@@ -0,0 +1,77 @@
+import 'isomorphic-fetch';
+import nock from 'nock';
+import get from 'get';
+import UserService from '../index';
+
+const serviceUrl = 'https://frontender.info/';
+
+describe('User API', () => {
+  it('should return list of post on get request: ', async () => {
+    nock(/[.]+/)
+      .get('/posts')
+      .reply(200, get);
+
+    const user = new UserService(serviceUrl);
+    const responce = await user.get();
+    expect(responce).toEqual(get);
+  });
+
+  it('should create post on post request: ', async () => {
+    const answer = {
+      title: 'My Title',
+      body: 'My Text',
+      id: 101,
+    };
+    nock(/[.]+/)
+      .post('/posts')
+      .reply(200, answer);
+
+    const user = new UserService(serviceUrl);
+    const responce = await user.post({});
+    expect(responce).toEqual(answer);
+  });
+
+  it('should return specific post on get request with post id: ', async () => {
+    const answer = {
+      id: 1,
+      userId: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body:
+        'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    };
+    nock(/[.]+/)
+      .get('/posts/1')
+      .reply(200, answer);
+
+    const user = new UserService(serviceUrl);
+    const responce = await user.details(1);
+    expect(responce).toEqual(answer);
+  });
+
+  it('should throw Not Found error if you request post with id that do not exists: ', async () => {
+    nock(/[.]+/)
+      .get('/posts/1')
+      .reply(404);
+
+    const user = new UserService(serviceUrl);
+    try {
+      await user.details(1);
+    } catch (error) {
+      expect(error.statusCode).toEqual(404);
+      expect(error.statusText).toEqual('Not Found');
+    }
+  });
+
+  it('should throw Server Responce error if you request post with id that do not exists: ', async () => {
+    nock(/[.]+/)
+      .get('/posts/1')
+      .reply(500);
+
+    const user = new UserService(serviceUrl);
+    try {
+      await user.details(1);
+    } catch (error) {
+      expect(error.statusCode).toEqual(500);
+    }
+  });
+});
---
+++ source/index.js
@@ -0,0 +1,119 @@
+import 'babel-polyfill';
+import { TMMicroServiceAPI, ErrorNotFound, ErrorServerResponse } from 'tm-service-api';
+
+/**
+ * User Service API
+ *
+ * @namespace UserService
+ * @module UserService
+ * @class
+ * @exports
+ *
+ * @param {string} url - service url
+ * @param {string | null} [token = null] - user access tocken if available
+ * @see https://frontender.info/
+ */
+export default class UserService extends TMMicroServiceAPI {
+  /**
+   * Object with class service messages
+   * @type {Object}
+   */
+  static messages = {};
+
+  /**
+   * Get users list
+   *
+   * @method get
+   * @async
+   * @public
+   * @memberof UserService
+   *
+   * @return {UserList} - array of posts
+   *
+   * @example <caption>Read users list</caption>
+   * (async () => {
+   *   const User = new UserService('https://user.frontender.info/');
+   *   const list = await User.get();
+   * })();
+   */
+  get = async () => {
+    const result = await super.request(`${this.url}`);
+    const json = await result.json();
+    return json;
+  };
+
+  /**
+   * Create user
+   *
+   * @method post
+   * @async
+   * @public
+   * @memberof UserService
+   *
+   * @param {string} title - post title
+   * @param {string} body - post body
+   * @return {User} - created user
+   *
+   * @example <caption>Create user</caption>
+   * (async () => {
+   *   const User = new UserService('https://frontender.info/');
+   *   const details = await User.post({});
+   * })();
+   */
+  post = async user => {
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+    const result = await super.request(`${this.url}`, options);
+    const json = await result.json();
+    return json;
+  };
+
+  /**
+   * User list object
+   *
+   * @namespace UserService
+   * @typedef {UserList} Array of users
+   *
+   * @property {Array.<User>} - array of users
+   */
+
+  /**
+   * User object
+   *
+   * @namespace UserService
+   * @typedef {User} User representation
+   */
+
+  /**
+   * Read specific user
+   *
+   * @method details
+   * @memberof UserService
+   * @async
+   * @public
+   *
+   * @param {number} id - user id
+   * @return {User} - user details
+   *
+   * @example <caption>Get user</caption>
+   * (async () => {
+   *   const User = new UserService('https://frontender.info/');
+   *   const details = await User.details(2);
+   * })();
+   */
+  details = async id => {
+    const response = await super.request(`${this.url}${id}`);
+    if (response.ok) {
+      const json = await response.json();
+      return json;
+    }
+    if (response.status === 404) throw new ErrorNotFound(UserService.messages.postNotFound);
+    throw new ErrorServerResponse(response.status, response.statusText);
+  };
+}
